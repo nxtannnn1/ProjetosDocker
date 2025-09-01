34e9937eab73d5c83145f2540447bacbc176384a
nxtannnn1 <natanwill.contatar@outlook.com>
2025-08-31 22:29:01 -0300

    Fix: exemplo calculadora + docker

---
+++ script.js
@@ -0,0 +1,16 @@
+function inserir(valor) {
+    document.getElementById("display").value += valor;
+}
+
+function limpar() {
+    document.getElementById("display").value = "";
+}
+
+function calcular() {
+    try {
+        let resultado = eval(document.getElementById("display").value);
+        document.getElementById("display").value = resultado;
+    } catch (e) {
+        alert("Expressão inválida!");
+    }
+}
